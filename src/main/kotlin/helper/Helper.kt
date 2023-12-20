15d8868ba1b9bceaa0fd6d591e00889557cbc14f
PP6464 <panthhp@outlook.com>
2023-12-20 15:38:08 +0000

    Remove redundant import

--- src/main/kotlin/helper/Helper.kt
+++ src/main/kotlin/helper/Helper.kt
@@ -1,7 +1,5 @@
 package helper
 
-import org.jetbrains.kotlinx.multik.api.*
-
 fun List<Double>.multiplyAll() : Double = reduce(Double::times)
 
 fun emptyActivations(size : Long) = (0..<size).map { 0.0 }
