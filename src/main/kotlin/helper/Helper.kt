f21188fa8578b3822a6b1e484e6c4261a9bd4e65
PP6464 <panthhp@outlook.com>
2023-12-20 15:37:29 +0000

    generate a list of empty activations for defaulting to

--- src/main/kotlin/helper/Helper.kt
+++ src/main/kotlin/helper/Helper.kt
@@ -1,3 +1,7 @@
 package helper
 
+import org.jetbrains.kotlinx.multik.api.*
+
 fun List<Double>.multiplyAll() : Double = reduce(Double::times)
+
+fun emptyActivations(size : Long) = (0..<size).map { 0.0 }
