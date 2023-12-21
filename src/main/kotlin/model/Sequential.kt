905616c9fe3f26e805f3d7db5720868b09349a18
PP6464 <panthhp@outlook.com>
2023-12-21 11:31:04 +0000

    Implement the run, save and load methods

--- src/main/kotlin/model/Sequential.kt
+++ src/main/kotlin/model/Sequential.kt
@@ -1,8 +1,9 @@
 package model
 
-import helper.emptyActivations
+import helper.*
 import layers.*
-import java.io.Serializable
+import org.jetbrains.kotlinx.multik.ndarray.data.*
+import java.io.*
 import kotlin.math.sqrt
 import kotlin.random.Random
 
@@ -14,8 +15,8 @@ import kotlin.random.Random
 
 data class Sequential(val layers : List<Layer>) : Serializable {
 	// weights contains a list for each layer pair, and each layer pair contains a list for each node of the second layer in the pair.
-	// Each int in the list is the weight of the connection from the corresponding first node to the corresponding second node.
-	val weights = mutableListOf<List<Double>>()
+	// Each double in the list for each node is the weight of the connection from the corresponding first node to the corresponding second node.
+	val weights = mutableListOf<List<List<Double>>>()
 	
 	// biases contains a list for each layer, where each element in the list is bias for that node
 	val biases = mutableListOf<List<Double>>()
@@ -26,14 +27,62 @@ data class Sequential(val layers : List<Layer>) : Serializable {
 		for (layerPair in layers.zipWithNext()) {
 			weights
 				.add(
-					(0..<layerPair.first.size).map {
-						Random.nextDouble(sqrt(6.0 / (layerPair.first.size + layerPair.second.size)))
+					(0..<layerPair.second.size).map {
+						(0..<layerPair.first.size).map {
+							Random.nextDouble(
+								- sqrt(6.0 / (layerPair.first.size + layerPair.second.size)),
+								sqrt(6.0 / (layerPair.first.size + layerPair.second.size))
+							)
+						}
+							.toMutableList()
 					}
-						.toMutableList()
 				)
-		} // Xavier/Glorot weights initialization
+		} // Xavier Glorot weights initialization
 		for ((index, layer) in layers.withIndex()) {
 			biases[index] = emptyActivations(layer.size) // Can use the same initialization to set biases to full of 0s
 		}
+	}
+	
+	/**
+	 * A function to save the model to a file
+	 * @param path: The path of the file to save the model to
+	 */
+	fun saveModel(path: String) {
+		ObjectOutputStream(FileOutputStream(path)).use {
+			it.writeObject(this)
+		}
+	}
+	
+	/**
+	 * A function that takes an input and outputs a prediction from the neural network
+	 * @param input: The input activation
+	 * @param returnIndex: Whether to return the index of the output node or the label of the output node (index used for training)
+	 */
+	fun run(input: NDArray<Double, Dim2>, returnIndex : Boolean = false) : Any {
+		layers[0].set2DActivations(input)
+		for (layerIndex in 1..layers.size) {
+			val activations = mutableListOf<Double>()
+			repeat(layers[layerIndex].size.toInt()) { nodeIndex ->
+				var total = 0.0
+				val bias = biases[layerIndex][nodeIndex]
+				repeat(layers[layerIndex - 1].size.toInt()) { prevLayerNodeIndex ->
+					val weight = weights[layerIndex - 1][nodeIndex][prevLayerNodeIndex]
+					total += weight * layers[layerIndex - 1].activations[prevLayerNodeIndex]
+				}
+				total += bias
+				activations += sigmoid(total)
+			}
+			layers[layerIndex].activations = activations
+		}
+		val maxActivationNodeIndex = layers.last().activations.withIndex().maxBy { it.value }.index
+		return if (returnIndex) maxActivationNodeIndex else (layers.last() as OutputLayer).outputs[maxActivationNodeIndex]
+	}
+	
+	companion object {
+		/**
+		 * A function to load the model from a file
+		 * @param path: The path of the file to load the model from
+		 */
+		fun loadObject(path: String) = ObjectInputStream(FileInputStream(path)).use { it.readObject() as Sequential }
 	}
 }
